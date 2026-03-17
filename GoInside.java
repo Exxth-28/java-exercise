3fbd2bca9ad8361910cb91624959f6fcc373d7bb
Exxth-28 <sry.pt2e@gmail.com>
2026-03-17 11:43:31 +0700

    Add files via upload

---
+++ GoInside.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class GoInside {
+    String goInside(String str, boolean front) {
+        if(str.isEmpty()) return "";
+        
+        if(str.length() == 1) {
+            return str;
+        }
+        if(front == true) {
+            return str.charAt(0) + str.charAt(str.length() - 1) + goInside(str.substring(1, str.length() - 1), false);
+        } else {
+            return str.charAt(str.length() - 1) + str.charAt(0) + goInside(str.substring(1, str.length() - 1), true);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String str = scan.next();
+        GoInside gi = new GoInside();
+        String answer = gi.goInside(str, true);
+        System.out.println(answer);
+        scan.close();
+    }
+}
